c9e9c00a65d2edab119f3c933caf60260e3f49da
DenSad81 <tagil81@mail.ru>
2024-04-13 23:27:55 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int _maxSpeed = 34;
+        uint _quantiyOfCar = 22;
+        float _actualHelth = 100.0f;
+        double _maxRadius = 44.5d;
+        byte _volumeOfFlat = 222;
+        char _keyForFire = 'A';
+        string _namePlayer = "Big Man";
+        bool _iaAlive = true;
+        long _timeFromStart = 0;
+        ulong _timeToFinish = 42424534354353;
+
+
+
+
+
+    }
+}
