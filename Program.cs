a0fad2b13088f00fc43c4e0e21972ff3eac00564
DenSad81 <tagil81@mail.ru>
2024-04-13 21:10:47 +0300

    Rebuild

--- Program.cs
+++ Program.cs
@@ -2,20 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        int _maxSpeed = 34;
-        uint _quantiyOfCar = 22;
-        float _actualHelth = 100.0f;
-        double _maxRadius = 44.5d;
-        byte _volumeOfFlat = 222;
-        char _keyForFire = 'A';
-        string _namePlayer = "Big Man";
-        bool _iaAlive = true;
-        long _timeFromStart = 0;
-        ulong _timeToFinish = 42424534354353;
-
-
-
-
-
+        int maxSpeed = 34;
+        uint quantityOfCar = 22;
+        float actualHelth = 100.0f;
+        double maxRadius = 44.5d;
+        byte volumeOfFlat = 222;
+        char keyForFire = 'A';
+        string namePlayer = "Big Man";
+        bool isAlive = true;
+        long timeFromStart = 0;
+        ulong timeToFinish = 42424534354353;
     }
 }
